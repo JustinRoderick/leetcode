f5c7f3e1fec6a0e3b1388ede4af25e4cc14879a7
JustinRoderick <justinroderick258@gmail.com>
2025-09-13 23:56:01 -0400

    commit

---
+++ python/medium/vowels_game_in_a_string.py
@@ -0,0 +1,6 @@
+# 3227. Vowels Game in a String
+
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        vowels = {"a", "e", "i", "o", "u"}
+        return any(char in vowels for char in s)
