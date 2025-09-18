17b4e3479df6dd7056563ec08bccfb8b7142ac58
JustinRoderick <justinroderick258@gmail.com>
2025-09-17 21:58:34 -0400

    commit

---
+++ python/easy/jewels_and_stones.py
@@ -0,0 +1,12 @@
+# 771. Jewels and Stones
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels = set(jewels)
+        count = 0
+
+        for stone in stones:
+            if stone in jewels:
+                count += 1
+        
+        return count
