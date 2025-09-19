42736e7aeb83ccb958af430caf39a4b4cabd25ff
JustinRoderick <justinroderick258@gmail.com>
2025-09-18 21:28:02 -0400

    commit

---
+++ python/medium/permutations.py
@@ -0,0 +1,21 @@
+# 46. Permutations
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        sol = []
+        n = len(nums)
+
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])
+                return
+
+            for num in nums:
+                if num not in sol:
+                    sol.append(num)
+                    backtrack()
+                    sol.pop()
+
+        backtrack()
+        return ans
