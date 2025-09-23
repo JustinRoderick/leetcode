c0162b921c1995d95af8e76f836bae81fbb79b33
JustinRoderick <justinroderick258@gmail.com>
2025-09-22 22:06:24 -0400

    solution using heap

--- python/medium/top_k_frequent_elements.py
+++ python/medium/top_k_frequent_elements.py
@@ -11,3 +11,18 @@ class Solution:
             ans.append(count[i][0])
         return ans
 
+# Solution using heap
+
+from collections import Counter
+from heapq import heappush, heappop
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        count = Counter(nums)
+        for key, value in count.items():
+            heappush(heap, (value, key))
+            if len(heap) > k:
+                heappop(heap)
+
+        return [freq[1] for freq in heap]
