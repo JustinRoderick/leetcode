a0c0717dcaa4e1028fc87931236cb3e16097ad1f
JustinRoderick <justinroderick258@gmail.com>
2025-10-07 17:17:26 -0400

    bfs and dfs

--- python/easy/same_tree.py
+++ python/easy/same_tree.py
@@ -6,6 +6,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# DFS
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
@@ -15,6 +17,31 @@ class Solution:
             elif((not p and q) or (p and not q)):
                 return False
             else:
-                return p.val == q.val and dfs(q.left, p.left) and dfs(q.right, p.right)
+                return p.val == q.val and dfs(p.left, q.left) and dfs(p.right, q.right)
         
         return dfs(p, q)
+
+# BFS
+from collections import deque
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        q1 = deque()
+        q2 = deque()
+        q1.append(p)
+        q2.append(q)
+        while q1 and q2:
+            curq1 = q1.popleft()
+            curq2 = q2.popleft()
+            if(not curq1 and not curq2):
+                continue
+            if(not curq1 and curq2) or (curq1 and not curq2) or (curq1.val != curq2.val):
+                return False
+
+            q1.append(curq1.left)
+            q1.append(curq1.right)
+            q2.append(curq2.left)
+            q2.append(curq2.right)
+
+        return True 
